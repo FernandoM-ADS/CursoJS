e8901153cb0ef0f36b921e016f20424a81b2b1e0
Fernando Mauricio <fernandomauricio.ads@gmail.com>
2022-12-28 19:43:14 -0300

    iniciando função

---
+++ funcoes/funcao_1.js
@@ -0,0 +1,24 @@
+let a = 4
+console.log(a)
+
+
+// Function Declaration
+function bomDia() {
+    console.log('Bom dia!')
+}
+
+bomDia()
+
+
+const boaTarde = function () {
+    console.log('Boa tarde!')
+}
+
+boaTarde()
+
+function somar(a, b) {
+    return a + b
+}
+
+const resultado = somar(3, 4)
+console.log(resultado)
